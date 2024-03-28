9784dbf35916b96466ef96327746232ef6064f79
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-28 20:22:21 +0100

    Arrays of opcodes created, instruction added

--- validate_commands.c
+++ validate_commands.c
@@ -2,8 +2,19 @@
 
 /**
  *validate_command - Checks for opcodes in the array
+ *@allopcodes: Array that holds all the opcodes
  */
 void validate_command(char *allopcodes[])
 {
 	allopcodes[] = {push, pall, pint, pop, swap, add, nop, sub, div, mul, mod, pchar, pstr, rotl, rotr, stack, queue};
+}
+
+void (*f)(stack_t **stack, unsigned int line_number)
+{
+	instruction_t array[] = {struct1, struct2, NULL};
+
+	for (i = 0; array[i] != NULL; i++)
+	{
+		if (strcmp(token[0], struct[i] == 0)
+	}
 }
