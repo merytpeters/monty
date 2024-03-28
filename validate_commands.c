3458bacfe77587b2ab0efbad1e1368c134cf06ec
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-28 20:07:38 +0100

    Arrays of opcodes created

---
+++ validate_commands.c
@@ -0,0 +1,9 @@
+#include "monty.h"
+
+/**
+ *validate_command - Checks for opcodes in the array
+ */
+void validate_command(char *allopcodes[])
+{
+	allOpcodes[] = {push, pall, pint, pop, swap, add, nop, sub, div, mul, mod, pchar, pstr, rotl, rotr, stack, queue};
+}
