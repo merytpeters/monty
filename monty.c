cded81b77b5cdbcea9aaf9bf3de5190d8b75def8
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-01 09:07:19 +0100

    using array in place of char pointer helped with the memory leak

--- monty.c
+++ monty.c
@@ -41,63 +41,6 @@ void parse_file(FILE *stream)
 }
 
 /**
- * parse_file - parses through files
- * @stream: File
- */
-/**
-  void parse_file(FILE *stream)
-  {
-  unsigned int line_count, len = 20;
-  char **tokz;
-  char *line = NULL;
-  int read_something;
-
-  line_count = 0;
-  do {
-  read_something = 0;
-  line = malloc(sizeof(char) * len);
-  line = fgets(line, len, stream);
-  if (line == NULL)
-  {
-  break;
-  }
-  else
-  {
-  read_something = 1;
-  line_count++;
-  tokz = tokenizer(line);
-  if (!tokz)
-  {
-  break;
-  }
-  if (tokz[0][0] == '#')
-  {
-  free_vec(tokz);
-  free(line);
-  continue;
-  }
-  if (!validate_command(tokz[0]))
-  {
-  fprintf(stderr, "L%d: unknown instruction %s\n", line_count, tokz[0]);
-  fclose(stream);
-  free(line);
-  free_vec(tokz);
-  free_stack(head);
-  exit(EXIT_FAILURE);
-  }
-  else
-  {
-  free(line);
-  opcode_delegator(tokz, line_count);
-  }
-  }
-  } while (read_something);
-  free(line);
-  free_stack(head);
-  }
-  */
-
-/**
  * main - checks the number of arguments
  * @argc: Number of arguments
  * @argv: argument vectors
